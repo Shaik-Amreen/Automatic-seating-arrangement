d2df295298d504c61cc63f79432fffcc53839279
Shaik-Amreen <amreenshaik40@gmail.com>
2020-08-06 12:27:47 +0530

    Updates

---
+++ precache-manifest.e88e8fb2269f61f5cde8c659a327fe65.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd3a2d18c68a2d67f354228a23665e6",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "a621074875dddbc894b3",
+    "url": "/client/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7d7d42bdc2d51afaf1e1",
+    "url": "/client/static/css/main.ddb6ed87.chunk.css"
+  },
+  {
+    "revision": "a621074875dddbc894b3",
+    "url": "/client/static/js/2.b4472d97.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/client/static/js/2.b4472d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7d42bdc2d51afaf1e1",
+    "url": "/client/static/js/main.62dc035b.chunk.js"
+  },
+  {
+    "revision": "d64b09180f5230249a46",
+    "url": "/client/static/js/runtime-main.1d4c076d.js"
+  },
+  {
+    "revision": "3d9c63990e3916236b05701fd84956ed",
+    "url": "/client/static/media/book.3d9c6399.png"
+  },
+  {
+    "revision": "663826f90769ff0feb1719333efea891",
+    "url": "/client/static/media/dred.663826f9.jfif"
+  },
+  {
+    "revision": "1877674b9f0978eca3ae39a151eb361d",
+    "url": "/client/static/media/header.1877674b.jpg"
+  },
+  {
+    "revision": "9bf9dc1ae5c717d1dfcdd8d61ed01861",
+    "url": "/client/static/media/main.9bf9dc1a.jpg"
+  },
+  {
+    "revision": "50d8831823a9579ec6e52237afe4126a",
+    "url": "/client/static/media/one.50d88318.jpg"
+  }
+]);
